9bbd9b4a1fab4c34ad143a36294439ab83f92ffa
Michael Gloudeman <gloudeman88@gmail.com>
2017-05-15 15:52:19 -0700

    fixed deprecated Class error

--- Gallery/frontend/Gallery.jsx
+++ Gallery/frontend/Gallery.jsx
@@ -1,24 +1,23 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var Router = require('react-router').Router;
-var Route = require('react-router').Route;
+var Router = require('react-router-dom').HashRouter;
+var Route = require('react-router-dom').Route;
 var IndexRoute = require('react-router').IndexRoute;
 var Link = require('react-router').Link;
-var HashHistory = require('react-router').hashHistory;
+
+import Navbar from './components/navbar';
 
 
-var App =  React.createClass({
-
-render: function () {
-
-}
-
-});
-
-var routes 
+class App extends React.Component {
+  render() {
+    return(<div>Hello
+      <Route path='/' component={Navbar}></Route>
+      </div>);
+  }
+};
 
 document.addEventListener("DOMContentLoaded", function (){
-  ReactDOM.render(<Router history={HashHistory} routes={routes}/>,
+  ReactDOM.render(<Router><App /></Router>,
                   document.getElementById("root"));
 });
