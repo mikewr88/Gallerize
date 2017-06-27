e272f4231536c844e2912fe765484ea3cdaff7f8
Michael Gloudeman <gloudeman88@gmail.com>
2017-06-27 00:22:21 -0700

    show page styling and editting function

--- Gallery/frontend/components/photo_show.jsx
+++ Gallery/frontend/components/photo_show.jsx
@@ -10,13 +10,15 @@ class PhotoShow extends React.Component {
     this.state = {
       photo: ImageStore.find(parseInt(this.props.match.params.photoId)),
       title: ImageStore.find(parseInt(this.props.match.params.photoId)).title,
-      description: ImageStore.find(parseInt(this.props.match.params.photoId)).description
+      description: ImageStore.find(parseInt(this.props.match.params.photoId)).description,
+      edit: false
     }
 
     this.getPhoto = this.getPhoto.bind(this);
     this.titleChange = this.titleChange.bind(this);
     this.descriptionChange = this.descriptionChange.bind(this);
     this.changeInfo = this.changeInfo.bind(this);
+    this.edit = this.edit.bind(this);
   }
 
   componentDidMount() {
@@ -38,18 +40,49 @@ class PhotoShow extends React.Component {
   changeInfo(e) {
     e.preventDefault();
     ImageActions.updateInfo({title: this.state.title, description: this.state.description, id: this.state.photo.id})
+    this.setState({edit: false});
+  }
+
+  edit(e) {
+    e.preventDefault();
+    this.setState({edit: true});
   }
 
   render() {
     var title = this.state.title;
     var description = this.state.description;
-    return (
-      <div id='photo-show-container'>
-        <img id='photo-show-img' src={this.state.photo.image_url}></img>
-        <label>Title:  <input type='text' value={title} onChange={this.titleChange} ></input></label>
-        <label>Description:  <input type='text' value={description} onChange={this.descriptionChange} ></input></label>
+    var ShowPage;
+    if (this.state.edit) {
+      ShowPage = (
+        <div id='photo-show-container'>
+          <img id='photo-show-img' src={this.state.photo.image_url}></img>
 
-        <button onClick={this.changeInfo} value='Done'>Done</button>
+          <div id='show-text-container'>
+            <button className='show-button' onClick={this.changeInfo} value='Save'>Save</button>
+
+            <label>Title:  <input type='text' value={title} onChange={this.titleChange} ></input></label>
+            <label>Description:  <input type='text' value={description} onChange={this.descriptionChange} ></input></label>
+
+          </div>
+        </div>
+      )
+    }else {
+      ShowPage = (
+        <div id='photo-show-container'>
+          <img id='photo-show-img' src={this.state.photo.image_url}></img>
+          <div id='show-text-container'>
+            <button className='show-button' onClick={this.edit}>Edit</button>
+
+            <div className='show-text'>{title}</div>
+            <div className='show-text'>{description}</div>
+
+          </div>
+        </div>
+      )
+    }
+    return (
+      <div id='show-page'>
+      {ShowPage}
       </div>
     );
   }
