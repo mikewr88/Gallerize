47d891b3f1cac5fc439f81eb87f6b53091934af3
Michael Gloudeman <gloudeman88@gmail.com>
2017-06-30 15:06:56 -0700

    fix landing page image load speed. small syle change

--- Gallery/frontend/constants/landing_photos.js
+++ Gallery/frontend/constants/landing_photos.js
@@ -1,10 +1,10 @@
 module.exports = {
-  1: 'https://static.pexels.com/photos/115045/pexels-photo-115045.jpeg',
-  2: 'https://static.pexels.com/photos/33936/pexels-photo.jpg',
-  3: 'https://static.pexels.com/photos/90824/pexels-photo-90824.jpeg',
-  4: 'https://static.pexels.com/photos/279315/pexels-photo-279315.jpeg',
-  5: 'https://static.pexels.com/photos/158385/lake-mcdonald-landscape-panorama-sunset-158385.jpeg',
-  6: 'https://static.pexels.com/photos/248174/pexels-photo-248174.jpeg',
-  7: 'https://static.pexels.com/photos/111255/pexels-photo-111255.jpeg',
-  8: 'https://static.pexels.com/photos/136166/pexels-photo-136166.jpeg'
+  1: 'http://res.cloudinary.com/pokecupid/image/upload/v1498848590/monumentvalley_hny8aa.jpg',
+  2: 'http://res.cloudinary.com/pokecupid/image/upload/v1498849433/cabin_oz9nhi.jpg',
+  3: 'http://res.cloudinary.com/pokecupid/image/upload/v1498848712/snowcapped_lbrhpa.jpg',
+  4: 'http://res.cloudinary.com/pokecupid/image/upload/v1498849445/beachhouses_viepqr.jpg',
+  5: 'http://res.cloudinary.com/pokecupid/image/upload/v1498848557/boats_tf7dic.jpg',
+  6: 'http://res.cloudinary.com/pokecupid/image/upload/v1498849444/town_ciytun.jpg',
+  7: 'http://res.cloudinary.com/pokecupid/image/upload/v1498848658/snowlake_v9rhho.jpg',
+  8: 'http://res.cloudinary.com/pokecupid/image/upload/v1498848603/lakereflection_hkbfeh.jpg'
 };
